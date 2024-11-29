6e7987af09a445bee3103a863f7ade9bcd98eb08
LucaasAndrade <lucasmarcelodeandradesilva@gmail.com>
2024-11-29 12:30:17 -0300

    feat: Serviço de listagem de produtos

---
+++ src/modules/products/services/CreateProductService.ts
@@ -0,0 +1,42 @@
+import { getCustomRepository } from "typeorm";
+import { ProductRepository } from "../typeorm/repositories/ProductsRepository";
+import AppError from "../../../shared/errors/appError";
+import Product from "../typeorm/entities/Product";
+
+
+interface IRequest{
+  name: string;
+  price: number;
+  quantity: number;
+}
+
+
+class CreateProductService {
+  public async execute({name, price, quantity}: IRequest): Promise<Product> {
+    const productsRepository = getCustomRepository(ProductRepository);
+
+    /*
+
+    Não permitir que o produto seja cadastrado caso já exista o nome
+
+    */
+
+    const productExists = await productsRepository.findByName(name);
+
+    if (productExists) {
+      throw new AppError('There is already one product with this name');
+    }
+
+    const product = productsRepository.create({
+      name,
+      price,
+      quantity,
+    });
+
+    await productsRepository.save(product);
+
+    return product
+  }
+}
+
+export default CreateProductService;
