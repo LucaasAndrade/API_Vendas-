9e29847bdaa41bc1585e673c532ff8e0573b4763
LucaasAndrade <lucasmarcelodeandradesilva@gmail.com>
2024-12-02 09:32:56 -0300

    feat: Tratamento das requisições de rota

--- src/modules/products/routes/products.routes.ts
+++ src/modules/products/routes/products.routes.ts
@@ -2,6 +2,8 @@
 
 import { Router } from 'express';
 import ProductsController from '../controllers/ProductsController';
+import { celebrate, Joi, Segments } from 'celebrate';
+
 
 const productsRouter = Router();
 const productsController = new ProductsController();
@@ -10,19 +12,54 @@ productsRouter.get('/', async (req, resp) => {
   await productsController.index(req, resp);
 });
 
-productsRouter.get('/:id', async (req, resp) => {
+productsRouter.get(
+  '/:id',
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required(),
+    }
+  }),
+  async (req, resp) => {
   await productsController.show(req, resp);
 });
 
-productsRouter.post('/', async (req, resp) => {
+productsRouter.post
+  ('/',
+    celebrate({
+      [Segments.BODY]: {
+        name: Joi.string().required(),
+        price: Joi.number().precision(2).required(),
+        quantity: Joi.number().required(),
+    }
+  }),
+  async (req, resp) => {
   await productsController.create(req, resp);
 });
 
-productsRouter.put('/:id', async (req, resp) => {
+productsRouter.put(
+  '/:id',
+    celebrate({
+      [Segments.BODY]: {
+        name: Joi.string().required(),
+        price: Joi.number().precision(2).required(),
+        quantity: Joi.number().required(),
+      },
+      [Segments.PARAMS]: {
+        id: Joi.string().uuid().required(),
+      },
+    }),
+  async (req, resp) => {
   await productsController.update(req, resp);
 });
 
-productsRouter.delete('/:id', async (req, resp) => {
+productsRouter.delete(
+  '/:id',
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required(),
+    }
+  }),
+  async (req, resp) => {
   await productsController.delete(req, resp);
 });
 
