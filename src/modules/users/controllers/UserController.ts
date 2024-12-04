25be0d04eeec30139435b2ca7c796303d4c43b65
lucaasandrade <lucasmarcelodeandradesilva@gmail.com>
2024-12-04 14:57:53 -0300

    feat: Controller de Usuários

---
+++ src/modules/users/controllers/UserController.ts
@@ -0,0 +1,28 @@
+import {Request, Response} from 'express';
+
+import CreateUserService from "../services/CreateUserService";
+import ListUserService from "../services/ListUserService";
+
+
+export default class UserController {
+    public async index(request: Request, response: Response): Promise<Response[] | any>{
+        const listUser = new ListUserService();
+        const users = await listUser.execute();
+
+        return users;
+    }
+
+    public async create(request: Request, response: Response): Promise<any>{
+        const {name, email, password} = request.body;
+
+        const createUser = new CreateUserService();
+        const user = createUser.execute({
+            name, 
+            email, 
+            password
+        });
+
+        return response.json(user);
+    }
+
+}
