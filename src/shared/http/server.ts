a49a37fa0a5c14d25ff38cd864e54a6075c921e0
lucaasandrade <lucasmarcelodeandradesilva@gmail.com>
2024-12-10 10:52:10 -0300

    feat: rota statica para melhor acesso às imagens

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -6,33 +6,37 @@ import routes from './routes';
 import AppError from '../errors/appError';
 import '../typeorm';
 import { errors } from 'celebrate';
+import upload from '../../config/upload';
 
 const app = express();
 
 app.use(cors());
+app.use('/files', express.static(upload.directory));
 app.use(express.json());
 
 app.use(routes);
 app.use(errors());
 
-app.use((
-  error: Error,
-  _request: Request,
-  response: Response,
-  next: NextFunction
-): any => {
-  if (error instanceof AppError) {
-    return response.status(error.statusCode).json({
+app.use(
+  (
+    error: Error,
+    _request: Request,
+    response: Response,
+    next: NextFunction,
+  ): any => {
+    if (error instanceof AppError) {
+      return response.status(error.statusCode).json({
+        status: 'error',
+        message: error.message,
+      });
+    }
+    return response.status(500).json({
       status: 'error',
-      message: error.message
+      message: error.message,
     });
-  };
-  return response.status(500).json({
-    status: 'error',
-    message: error.message
-  })
-});
+  },
+);
 
 app.listen(3030, () => {
-  console.log("Servidor online! Porta: " + 3030);
-})
+  console.log('Servidor online! Porta: ' + 3030);
+});
