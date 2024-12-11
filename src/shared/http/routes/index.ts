935b9e72a18113d26617aa9bb24edff64da8eb9a
lucaasandrade <lucasmarcelodeandradesilva@gmail.com>
2024-12-11 13:38:25 -0300

    feat: Rota de Forgot password

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,13 +1,16 @@
 import { Router, Request, Response } from 'express';
+
 import productsRouter from '../../../modules/products/routes/products.routes';
 import usersRouter from '../../../modules/users/routes/user.routes';
 import sessionRouter from '../../../modules/users/routes/session.routes';
+import passwordRouter from '../../../modules/users/routes/password.routes';
 
 const routes = Router();
 
 routes.use('/products', productsRouter);
 routes.use('/users', usersRouter);
 routes.use('/sessions', sessionRouter);
+routes.use('/password', passwordRouter);
 
 routes.get('/', (_request: Request, response: Response): any => {
   console.log(typeof response);
