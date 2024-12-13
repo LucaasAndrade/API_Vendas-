b6cbd208659412bc3fe7e76e8a836ed4a10f6685
lucaasandrade <lucasmarcelodeandradesilva@gmail.com>
2024-12-13 12:01:47 -0300

    feat: Rotas de perfil do usuário

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -4,6 +4,7 @@ import productsRouter from '../../../modules/products/routes/products.routes';
 import usersRouter from '../../../modules/users/routes/user.routes';
 import sessionRouter from '../../../modules/users/routes/session.routes';
 import passwordRouter from '../../../modules/users/routes/password.routes';
+import profileRouter from '../../../modules/users/routes/profle.routes';
 
 const routes = Router();
 
@@ -11,6 +12,7 @@ routes.use('/products', productsRouter);
 routes.use('/users', usersRouter);
 routes.use('/sessions', sessionRouter);
 routes.use('/password', passwordRouter);
+routes.use('/profile', profileRouter);
 
 routes.get('/', (_request: Request, response: Response): any => {
   console.log(typeof response);
